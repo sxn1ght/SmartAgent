d6d94355d0091aa0e406ed0a7984d753b3772924
sxn1ght <sxn1ght7199@gmail.com>
2016-03-12 23:27:23 +0800

    rebuild day1

--- app/src/main/java/com/ksu/smartagent/MainActivity.java
+++ app/src/main/java/com/ksu/smartagent/MainActivity.java
@@ -6,33 +6,24 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private StatusBarColorChange statusBarColorChange; //宣告更改狀態列顏色的物件
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        init(); //呼叫初始化Function
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    //初始化Function
+    private void init(){
+        statusBarColorChange = new StatusBarColorChange(this);
+
+        toolbar.setTitle("");
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 }
